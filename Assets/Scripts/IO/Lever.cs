1aa56ad1e88a40a645b59bda8f18c31e902e57d8
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2021-06-14 01:17:02 +1000

    Levers, pressure plates, UI and level updates

--- Assets/Scripts/IO/Lever.cs
+++ Assets/Scripts/IO/Lever.cs
@@ -1,10 +1,19 @@
+using UnityEngine;
+
 public class Lever : InputObject
 {
 	bool on = false;
+	public Transform lever;
+
+	private void Awake()
+	{
+		lever.localRotation = Quaternion.Euler(-75, 0, 0);
+	}
 
 	public void Toggle()
 	{
 		on = !on;
+		lever.localRotation = Quaternion.Euler(-lever.localRotation.eulerAngles.x, 0, 0);
 		if (on)
 		{
 			Interact();
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using static Fade;
 
 public class LevelManager : MonoBehaviour
 {
 	Goal[] goals;
 
+	public CanvasGroup victoryScreen;
+
+	public RectTransform prompt;
+	public AnimationCurve promptCurve;
+	float promptTime;
+
+	public Object nextLevel;
+
 	public Object[] levelsToUnlock;
+
 	private void Awake()
 	{
 		goals = GetComponentsInChildren<Goal>();
+	}
+
+	private void Update()
+	{
+		if (prompt)
+		{
+			prompt.anchoredPosition = new Vector2(promptCurve.Evaluate(promptTime), 0);
+			promptTime += Time.deltaTime;
+		}
+
+		if (Input.GetButtonDown("Restart"))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
 	}
 
 	public void CheckComplete()
@@ -20,6 +45,8 @@ public class LevelManager : MonoBehaviour
 			if (!Physics.CheckBox(goal.transform.position + new Vector3(0, 0.3f, 0), new Vector3(0.6f, 0.6f, 0.6f), Quaternion.identity, 1 << goal.playerLayer)) return;
 		}
 		// Victory
+
+		FindObjectsOfType<PlayerController>().ToList().ForEach(p => p.enabled = false);
 		PlayerPrefs.SetString(SceneManager.GetActiveScene().name, "complete");
 		foreach (Object level in levelsToUnlock)
 		{
@@ -28,5 +55,12 @@ public class LevelManager : MonoBehaviour
 				PlayerPrefs.SetString(level.name, "unlocked");
 			}
 		}
+
+		StartCoroutine(FadeElement(victoryScreen, 0.5f, 0, 1, 0));
+
 	}
+
+	public void LoadNextLevel() => SceneManager.LoadScene(nextLevel.name);
+
+	public void ToMenu() => SceneManager.LoadScene(0);
 }
